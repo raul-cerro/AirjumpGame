b82764417351783cd4ca7a072bdd5505000d08ae
raul-cerro <79109923+raul-cerro@users.noreply.github.com>
2021-04-01 13:30:36 +0200

    Create MenuSettings.cs

---
+++ .github/workflows/MenuSettings.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+
+   public void SetVolume (float volume)
+   {
+       audioMixer.SetFloat("MasterVolume", volume);
+   }
+
+   public void SetFullscreen (bool isFullscreen)
+   {
+       Screen.fullScreen = isFullscreen;
+   }
