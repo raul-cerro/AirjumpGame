aa51947325a489b909d16e97d2a1c3e225690e14
raul-cerro <79109923+raul-cerro@users.noreply.github.com>
2021-04-01 13:28:54 +0200

    Create MainMenu.cs

---
+++ .github/workflows/MainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+   public void PlayGame()
+   {
+       SceneManager.LoadScene("Tutorial");
+   }
+    public void Nivel1(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+    public void Nivel2(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+
+    }
+    public void Nivel3(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+
+    }
+    public void Nivel4(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene("StartMenu");
+    }
+   public void QuitGame(){
+       Application.Quit();
+   }
+   public void NextLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+   }
+   
+   
+}
