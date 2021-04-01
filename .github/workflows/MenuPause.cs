a3e9b4d349884a0c6895eaad8dd4a871d35fd49d
raul-cerro <79109923+raul-cerro@users.noreply.github.com>
2021-04-01 13:36:47 +0200

    Update MenuPause.cs

--- .github/workflows/MenuPause.cs
+++ .github/workflows/MenuPause.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+public class MenuPause : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
