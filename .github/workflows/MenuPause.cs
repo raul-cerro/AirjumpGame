fdddc8cbf2548723594ab3deb235968b75f0d6c3
raul-cerro <79109923+raul-cerro@users.noreply.github.com>
2021-04-01 13:32:01 +0200

    Create MenuPause.cs

---
+++ .github/workflows/MenuPause.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject Player;
+    public GameObject Timer;
+    public static bool Finished = false;
+
+    // Update is called once per frame
+    void Update()
+    {   if( Finished == true){
+            
+        }else{
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {  
+             Pause ();
+            }
+        }
+    }
+    
+    public void Resume (){
+        pauseMenuUI.SetActive(false);
+        Player.SetActive(true);
+        Timer.SetActive(true);
+    
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void Pause (){ 
+        Cursor.visible = true;
+        pauseMenuUI.SetActive(true);
+        Player.SetActive(false);
+        Timer.SetActive(false);        
+        Time.timeScale = 0f;
+        GameIsPaused = true; 
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
