7ec26cc6498f7574cb4b75e99ddbffff5c5cd177
raul-cerro <79109923+raul-cerro@users.noreply.github.com>
2021-04-01 13:36:10 +0200

    Update MenuEnd.cs

--- .github/workflows/MenuEnd.cs
+++ .github/workflows/MenuEnd.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndMenu : MonoBehaviour
+public class MenuEnd : MonoBehaviour
 {
     public void LoadMenu(){
         SceneManager.LoadScene("StartMenu");
